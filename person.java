01b14f0e254fb5804681e76fddae52244a2078a0
Punkhurst <110967123+VsMayfly@users.noreply.github.com>
2023-04-07 10:57:51 +0800

    Create person.java

---
+++ person.java
@@ -0,0 +1,44 @@
+package step3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User、loser、coder 和 programmer
+*/
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		//main 方法必须从键盘读取字符串
+		Person person = null;
+		String key = null;
+     
+		while (true) {
+			key = reader.readLine();
+			if (!(key.equals("user") || key.equals("loser") || key.equals("coder") || key.equals("programmer")))
+			
+            /*
+            补全代码
+            //读取任何与上述内容不匹配的字符串后停止读取。
+                       
+			
+
+			
+            */                
+		}
+	}
+
+	public static void doWork(Person person) {
+		
+        /*
+            补全代码
+            调用 doNothing() 方法（如果人是 Loser）。
+            调用 writeCode() 方法（如果人是 Coder）。
+            调用 enjoy() 方法（如果人是 Programmer）    
+       
+            
+        */      
+		
+	}
+}
